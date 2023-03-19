0998f5f872b1353bc9f815b5ddb90eb356e2f891
malka <tahiro9011@gmail.com>
2023-03-19 14:13:58 -0500

    Created initial homepage

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( <div>
+        <h1>Malka Tahiro</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+        </ul>
+    </div> );
+}
+ 
+export default Navbar;
