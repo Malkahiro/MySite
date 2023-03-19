1043a3601a99269a2aabc29b030b65b558b41649
malka <tahiro9011@gmail.com>
2023-03-19 14:36:17 -0500

    created inital about me page

---
+++ src/Components/About.js
@@ -0,0 +1,20 @@
+const About = () => {
+    return ( <div>
+        <h1>About Me</h1>
+        <p>
+            Hello, My name is Malka and I am a FullStack Web Devloper that has 
+            recently graduated from Fullstack Academy's Web Delevoper Bootcamp.
+        </p>
+
+        <p>
+            I can use the PERN Stack (Postgresql, Express, React, and Node) to create amazing looking
+            and amazing running websites.
+        </p>
+
+        <p>
+            While I am always learning new things, I am confident in my ability to tackle any challenge that is thrown at me in the future.
+        </p>
+    </div> );
+}
+ 
+export default About;
